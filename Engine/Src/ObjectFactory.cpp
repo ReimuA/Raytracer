8cfb7b86fda4f2a2c4c9864254eab79bc71b76b6
Reimua <thibault.dolley@epitech.eu>
2018-07-05 17:26:30 +0200

    Updated factory

--- Engine/Src/ObjectFactory.cpp
+++ Engine/Src/ObjectFactory.cpp
@@ -17,14 +17,12 @@ ObjectFactory::~ObjectFactory()
 
 void ObjectFactory::feed(const std::string &path)
 {
-	IObject *(*createObject)();
+	IObject *(*createObject)(const std::map<std::string, std::string>);
 	std::string(*getObjectName)();
 	void *handler = DlHelper::dlOpen(path);
 	*(void **)(&createObject) = (IObject *)DlHelper::dlSym(handler, "createObject");
 	*(void **)(&getObjectName) = (std::string *)DlHelper::dlSym(handler, "getObjectName");
 
-	std::string str = getObjectName();
-	IObject *obj = createObject();
-	obj->dump();
-	std::cout << str << std::endl;
+	_factory[getObjectName()] = createObject;
+	std::cout << getObjectName() << std::endl;
 }
